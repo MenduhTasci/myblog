b9838fa9f6f0b11a75a7aa7187204c527ef65a7c
Menduh Tasci <tascimenduh@gmail.com>
2018-10-20 13:09:25 +0300

    login eklendi

---
+++ app/src/main/java/com/memo/myblog/Consts.java
@@ -0,0 +1,6 @@
+package com.memo.myblog;
+
+public class Consts {
+    public static final Integer SPLASH_DELAY=300;
+
+}
---
+++ app/src/main/java/com/memo/myblog/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.memo.myblog;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+
+        }, 300);
+    }
+}
